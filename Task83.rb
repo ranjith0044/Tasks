4976eee895ca492c787f79f68bec346d6d353079
ranjith <ranjithkumar.immanni@amzur.com>
2012-07-13 17:31:27 +0530

    adding file

---
+++ Task83.rb
@@ -0,0 +1,37 @@
+class Rect
+
+    def initialize()
+        
+   print "enter height and width of a rectangle:"
+   
+   
+    h=gets.chomp.to_i
+    w=gets.chomp.to_i
+        @h=h
+        @w=w
+    end
+    
+    def draw_rect
+    
+        for i in  1..@w
+        print "*"
+        end
+        print "\n"
+        
+        for j in 1..@h-1
+        print "*"
+        for k in 2..@w-1
+        print " "
+        end
+        print "*"
+        print "\n"
+        end
+        
+        for i in 1..@w
+        print "*"
+        end
+    end
+end
+    r=Rect.new
+    r.draw_rect
+    print "\n\n"
